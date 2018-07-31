2022a76e84ac3d912c052ce06b28cd351596277c
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-07-31 09:17:31 +0200

    header prototype
    Signed-off-by: Cyrill Bolliger <cyrill.bolliger@gmail.com>

--- wordpress/wp-content/themes/les-verts/index.php
+++ wordpress/wp-content/themes/les-verts/index.php
@@ -14,7 +14,6 @@
  */
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
-$context['foo'] = 'bar';
 $templates = array( 'index.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'home.twig' );
