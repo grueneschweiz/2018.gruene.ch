f0226073b3f53d7263158de3e5c4243c6e404d02
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-08-02 15:39:50 +0200

    navigation ready
    Signed-off-by: Cyrill Bolliger <cyrill.bolliger@gmail.com>

--- wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
+++ wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
@@ -2,7 +2,6 @@
 
 namespace SUPT;
 
-use \TimberMenu;
 
 class Navigation_controller {
 	
@@ -12,17 +11,51 @@ class Navigation_controller {
 	
 	public static function add_to_context( $context ) {
 		
-		$context['menu']['main']     = new TimberMenu( 'main-nav', [ 'depth' => 3 ] );
-		$context['menu']['language'] = new TimberMenu( 'language-nav', [ 'depth' => 1 ] );
+		$menu = new \TimberMenu( 'main-nav', [ 'depth' => 3 ] );
+		self::prepare_featured_items( $menu->items );
+		
+		$context['menu']['main']     = $menu;
+		$context['menu']['language'] = new \TimberMenu( 'language-nav', [ 'depth' => 1 ] );
 		$context['menu']['cta']      = [ 'label' => 'mitmachen', 'link' => '#' ]; // todo: get them from the customizer
 		
 		$img_id                  = get_theme_mod( Customizer\Logo::SETTING_LOGO_DARK, false );
 		$context['menu']['logo'] = [
-			'image' => new \TimberImage( $img_id ),
-			'srcset' => [2, 3],
-			'resize' => [116],
+			'image'  => new \TimberImage( $img_id ),
+			'srcset' => [ 2, 3 ],
+			'resize' => [ 116 ],
 		];
 		
 		return $context;
+	}
+	
+	/**
+	 * Walk the menu recursively and transform all category ids that belong to
+	 * a featured image into a TimberTerm and all images in a TimberImage.
+	 *
+	 * @param $timberMenu
+	 */
+	public static function prepare_featured_items( &$timberMenu ) {
+		if ( ! is_array( $timberMenu ) && ! is_object( $timberMenu ) ) {
+			return;
+		}
+		
+		foreach ( $timberMenu as &$item ) {
+			if ( is_array( $item ) ) {
+				foreach ( $item as &$i ) {
+					self::prepare_featured_items( $i );
+				}
+			}
+			
+			if ( $item instanceof \Timber\MenuItem ) {
+				if ( $item->featured_menu_item ) {
+					$item->category = new \TimberTerm( $item->category );
+					$item->image    = new \TimberImage( $item->image );
+				}
+				
+				if ( $item->children ) {
+					self::prepare_featured_items( $item->children );
+				}
+			}
+		}
 	}
 }
