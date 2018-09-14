6190b1ee5218c56ba7bc39fd69f372869b1ddec3
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-09-14 11:58:44 +0200

    press release wp output integration

--- wordpress/wp-content/themes/les-verts/single_press_release.php
+++ wordpress/wp-content/themes/les-verts/single_press_release.php
@@ -3,4 +3,14 @@
  * Template Name: Press Release
  * Template Post Type: post
  */
-?>
+
+$context         = Timber::get_context();
+$post            = Timber::query_post();
+$context['post'] = $post;
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ),
+		$context );
+}
