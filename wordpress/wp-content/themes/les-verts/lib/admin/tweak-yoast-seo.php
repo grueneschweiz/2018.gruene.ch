fc3ec466b476b1d43b581fe6993e2afa3edbe275
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-10-09 14:42:37 +0200

    improve breadcrumbs and make them work with single events

--- wordpress/wp-content/themes/les-verts/lib/admin/tweak-yoast-seo.php
+++ wordpress/wp-content/themes/les-verts/lib/admin/tweak-yoast-seo.php
@@ -4,28 +4,48 @@
  * Lower the Yoast Metabox to be shown after the normal content
  * when editing posts/pages
  */
-add_filter( 'wpseo_metabox_prio', function() {
+add_filter( 'wpseo_metabox_prio', function () {
 	return 'low';
-});
+} );
+
+
+add_filter( 'wpseo_breadcrumb_output_wrapper', function () {
+	return 'div';
+} );
 
 
 /**
  * Add a class to the second last breadcrumb so we can apply good mobile styles
  */
 add_filter( 'wpseo_breadcrumb_output', function ( $out ) {
-	$last_class  = 'breadcrumb_last';
-	$element     = '<span>';
-	$replacement = '<span class="breadcrumb_second_last">';
+	
+	// add breadcrumbs for single events
+	if ( is_singular( 'tribe_events' ) ) {
+		$queried_object  = get_queried_object();
+		$event_list_obj  = get_post_type_object( 'tribe_events' );
+		$event_list_link = get_post_type_archive_link( 'tribe_events' );
+		
+		$replace = '  <span><a href="' . $event_list_link . '">' . $event_list_obj->labels->name . '</a>  <span class="breadcrumb_last">' . $queried_object->post_title . '</span></span></span></div>';
+		
+		$out = str_replace( '</div>', $replace, $out );
+	}
+	
+	$last_class = 'breadcrumb_last';
 	
 	// get position of 'breadcrumb_last'
 	$last_pos = strpos( $out, $last_class );
 	
-	// search for the last <span> before 'breadcrumb_last' to get second last element
-	$before       = substr( $out, 0, $last_pos );
-	$last_element = strrpos( $before, $element );
+	// bail early if there is only one element
+	if ( false === $last_pos ) {
+		return $out;
+	}
 	
-	// replace the <span> with <span class="breadcrumb_second_last">
-	$out = substr_replace( $out, $replacement, $last_element, strlen( $element ) );
+	// search for the last '<a ' before 'breadcrumb_last' to get second last element
+	$before       = substr( $out, 0, $last_pos );
+	$last_element = strrpos( $before, '<a ' );
+	
+	// replace the '<a ' with '<a class="breadcrumb_second_last" '
+	$out = substr_replace( $out, '<a class="breadcrumb_second_last" ', $last_element, 3 );
 	
 	return $out;
 } );
@@ -45,7 +65,7 @@ add_filter( 'wpseo_breadcrumb_separator', function () {
  * - The user shall not disable the breadcrumbs for blog pages
  * - The separator will be filtered and has therefor no effect
  */
-add_action( 'customize_register', function($wp_customize) {
-	$wp_customize->remove_control('wpseo-breadcrumbs-display-blog-page');
-	$wp_customize->remove_control('wpseo-breadcrumbs-separator');
+add_action( 'customize_register', function ( $wp_customize ) {
+	$wp_customize->remove_control( 'wpseo-breadcrumbs-display-blog-page' );
+	$wp_customize->remove_control( 'wpseo-breadcrumbs-separator' );
 }, 20 );
