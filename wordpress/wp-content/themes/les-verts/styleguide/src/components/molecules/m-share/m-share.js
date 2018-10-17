d3e866b03eb1f59199ab37d70942e78d452c7667
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-10-17 18:21:38 +0200

    new share style

--- wordpress/wp-content/themes/les-verts/styleguide/src/components/molecules/m-share/m-share.js
+++ wordpress/wp-content/themes/les-verts/styleguide/src/components/molecules/m-share/m-share.js
@@ -1,6 +1,7 @@
 import BaseView from './../../../js/base-view';
 
 const MOBILE_SELECTOR = '.m-share__mobile';
+const CLOSE_SELECTOR = '.m-share__close';
 const BUTTONS_SELECTOR = '.m-share__buttons';
 
 const OPEN_STATE = 'is-open';
@@ -8,21 +9,19 @@ const OPEN_STATE = 'is-open';
 export default class MShare extends BaseView {
 	initialize() {
 		this.buttons = this.getScopedElement( BUTTONS_SELECTOR );
-
-		this.isOpen = false;
 	}
 
 	bind() {
 		super.bind();
 
 		this.on( 'click', MOBILE_SELECTOR, () => {
-			if (this.isOpen) {
-				this.removeClass( this.buttons, OPEN_STATE );
-			} else {
-				this.addClass( this.buttons, OPEN_STATE );
-			}
+			this.addClass( this.buttons, OPEN_STATE );
+			this.addClass( OPEN_STATE );
+		} );
 
-			this.isOpen = ! this.isOpen;
+		this.on( 'click', CLOSE_SELECTOR, () => {
+			this.removeClass( this.buttons, OPEN_STATE );
+			this.removeClass( OPEN_STATE );
 		} );
 	}
 }
