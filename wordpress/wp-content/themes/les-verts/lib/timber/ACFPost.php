3399cca43d0a744890fba17dd7a8a3dd34d42c5a
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-10-30 17:04:20 +0100

    preset sm post text

--- wordpress/wp-content/themes/les-verts/lib/timber/ACFPost.php
+++ wordpress/wp-content/themes/les-verts/lib/timber/ACFPost.php
@@ -11,6 +11,7 @@ namespace SUPT;
 class ACFPost extends \TimberPost {
 	private $__excerpt;
 	private $__fullExcerpt;
+	private $__twitterName;
 	
 	public function excerpt( $length = 280 ) {
 		// bail early to ensure we'll never compute it twice
@@ -18,7 +19,7 @@ class ACFPost extends \TimberPost {
 			return $this->__excerpt;
 		}
 		
-		$this->__excerpt = $this->limit_length( $this->fullExcerpt(), $length );
+		$this->__excerpt = $this->limit_length( $this->full_excerpt(), $length );
 		
 		return $this->__excerpt;
 	}
@@ -33,7 +34,7 @@ class ACFPost extends \TimberPost {
 		return $text;
 	}
 	
-	public function fullExcerpt() {
+	public function full_excerpt() {
 		// bail early to ensure we'll never compute it twice
 		if ( $this->__fullExcerpt ) {
 			return $this->__fullExcerpt;
@@ -48,7 +49,7 @@ class ACFPost extends \TimberPost {
 		}
 		
 		if ( empty( $tmp ) ) {
-			$tmp = $this->generateExcerpt();
+			$tmp = $this->generate_excerpt();
 		}
 		
 		$this->__fullExcerpt = $tmp;
@@ -56,10 +57,11 @@ class ACFPost extends \TimberPost {
 		return $this->__fullExcerpt;
 	}
 	
-	private function generateExcerpt() {
+	private function generate_excerpt() {
 		if ( empty( $this->content ) ) {
 			return '';
 		}
+		
 		foreach ( $this->content as $block ) {
 			if ( 'text' === $block['acf_fc_layout'] ) {
 				$text = strip_shortcodes( $block['text'] );
@@ -69,5 +71,24 @@ class ACFPost extends \TimberPost {
 				return $text;
 			}
 		}
+		
+		return '';
+	}
+	
+	public function twitter_name() {
+		// bail early to ensure we'll never compute it twice
+		if ( null !== $this->__twitterName ) {
+			return $this->__twitterName;
+		}
+		
+		if ( class_exists( '\WPSEO_Options' ) ) {
+			$twitter_name        = apply_filters( 'wpseo_twitter_site', \WPSEO_Options::get( 'twitter_site' ) );
+			$twitter_name        = trim( $twitter_name, "@\t\n\r\0\x0B" ); // remove @ (if there is one) and trim
+			$this->__twitterName = $twitter_name;
+		} else {
+			$this->__twitterName = '';
+		}
+		
+		return $this->__twitterName;
 	}
 }
