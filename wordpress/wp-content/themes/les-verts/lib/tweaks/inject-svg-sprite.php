86abd527836853b3be9f09ed27dfe0bc28ee277e
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-11-10 14:34:41 +0100

    fix: path to svg sprite

--- wordpress/wp-content/themes/les-verts/lib/tweaks/inject-svg-sprite.php
+++ wordpress/wp-content/themes/les-verts/lib/tweaks/inject-svg-sprite.php
@@ -4,5 +4,5 @@
  * Include the SVG icon sprite in the footer
  */
 add_action( 'wp_footer', function() {
-	echo '<div style="display: none">'. file_get_contents( get_stylesheet_directory_uri() . '/static/icons.svg' ) .'</div>';
+	echo '<div style="display: none">'. file_get_contents( get_stylesheet_directory() . '/static/icons.svg' ) .'</div>';
 } );
