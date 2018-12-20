6670603db44e071f9261733dabd01ce7f88221f3
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-12-20 11:02:53 +0100

    change capabilities to manage_options

--- wordpress/wp-content/themes/les-verts/lib/form/settings-page.php
+++ wordpress/wp-content/themes/les-verts/lib/form/settings-page.php
@@ -9,7 +9,7 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		'menu_title'  => 'Settings',
 		'menu_slug'   => 'form-general-settings',
 		'parent_slug' => 'edit.php?post_type=' . \SUPT\FormType::MODEL_NAME,
-		'capability'  => 'edit_generaloptions',
+		'capability'  => 'manage_options',
 	) );
 	
 }
