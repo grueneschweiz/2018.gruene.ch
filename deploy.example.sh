b618c63aa1a21c138ee37c9b721628abffb8464e
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2019-01-15 14:49:15 +0100

    add deploy example file

--- deploy.example.sh
+++ deploy.example.sh
@@ -4,8 +4,13 @@
 # # Custom theme
 # './wordpress/wp-content/themes/les-verts/**/*',
 # '!./wordpress/wp-content/themes/les-verts/vendor/**/*',
+# '!./wordpress/wp-content/themes/les-verts/styleguide/**/*',
+# './wordpress/wp-content/themes/les-verts/styleguide/dist/static/**/*',
+# '!./wordpress/wp-content/themes/les-verts/styleguide/dist/static/styleguide**/*',
 # # Specific plugins (not installable by wp-cli)
 # './wordpress/wp-content/plugins/advanced-custom-fields-pro/**/*'
+# './wordpress/wp-content/plugins/polylang-pro/**/*'
+# './wordpress/wp-content/plugins/searchwp/**/*'
 
 # NOTES:
 # We assume wp-cli & core wordpress files are already installed. If not, run the script `setup-server.sh` on the server before.
@@ -13,13 +18,18 @@
 # ================================================================
 # ============ EDIT THE VALUES OF THE VARIABLES BELOW ============
 
-SSH_USER='SSH_USERNAME'
-SSH_HOST='SSH_HOST'
-SSH_PATH='~/SITE_PATH'
+SERVER_USER_DIR='/home/'
 
-SITE_URL='locahost'
-SITE_PATH="./web"
-SITE_THEME_PATH='./web/wp-content/themes/les-verts'
+SSH_USER='user'
+SSH_HOST='mydomain.ch'
+SSH_PATH="./www/mydomain.ch"
+
+SITE_URL='mydomain.ch'
+SITE_PATH=${SSH_PATH}
+SITE_THEME_PATH="${SITE_PATH}/wp-content/themes/les-verts"
+
+COMPOSER_COMMAND="${SERVER_USER_DIR}/bin/composer"
+WP_COMMAND="${SERVER_USER_DIR}/bin/wp"
 
 # ====== STOP TO EDIT HERE or continue at your own risks 💣 ======
 # ================================================================
@@ -28,9 +38,11 @@ SITE_THEME_PATH='./web/wp-content/themes/les-verts'
 yarn build
 
 # Show what will be deployed and ask for confirmation
-rsync -auvrzn --exclude='vendor' --exclude='styleguide' ./wordpress/wp-content/themes/les-verts "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/themes/"
-rsync -auvrzn ./wordpress/wp-content/plugins/advanced-custom-fields-pro "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/plugins/"
-rsync -auvrzn ./wordpress/wp-content/plugins/polylang-pro "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/plugins/"
+rsync -auvrzn --exclude='vendor' --exclude='styleguide' ./wordpress/wp-content/themes/les-verts "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/themes/"
+rsync -auvrzn --exclude='styleguide' ./wordpress/wp-content/themes/les-verts/styleguide/dist/static "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/themes/les-verts/"
+rsync -auvrzn ./wordpress/wp-content/plugins/advanced-custom-fields-pro "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
+rsync -auvrzn ./wordpress/wp-content/plugins/polylang-pro "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
+rsync -auvrzn ./wordpress/wp-content/plugins/searchwp "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
 
 read -p "The above files will be deployed. Continue? [y/n] " -n 1 -r
 echo
@@ -41,15 +53,16 @@ then
 fi
 
 # Copy everything needed
-rsync -auvrz --exclude='vendor' --exclude='styleguide' ./wordpress/wp-content/themes/les-verts "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/themes/"
-rsync -auvrz ./wordpress/wp-content/plugins/advanced-custom-fields-pro "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/plugins/"
-rsync -auvrz ./wordpress/wp-content/plugins/polylang-pro "$SSH_USER@$SSH_HOST:$SSH_PATH/wp-content/plugins/"
+rsync -auvrz --exclude='vendor' --exclude='styleguide' ./wordpress/wp-content/themes/les-verts "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/themes/"
+rsync -auvrz --exclude='styleguide' ./wordpress/wp-content/themes/les-verts/styleguide/dist/static "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/themes/les-verts/"
+rsync -auvrz ./wordpress/wp-content/plugins/advanced-custom-fields-pro "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
+rsync -auvrz ./wordpress/wp-content/plugins/polylang-pro "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
+rsync -auvrz ./wordpress/wp-content/plugins/searchwp "${SSH_USER}@${SSH_HOST}:${SSH_PATH}/wp-content/plugins/"
 
 # Install composer dependencies
-ssh "$SSH_USER@$SSH_HOST" "/opt/php7.1/bin/php -d allow_url_fopen=On composer.phar install --working-dir=$SITE_THEME_PATH"
-
+ssh "$SSH_USER@$SSH_HOST" "$COMPOSER_COMMAND install --working-dir=$SITE_THEME_PATH"
 
 # Run bootstrap script
-ssh $SSH_USER@$SSH_HOST "cd ~ && WPCLI='/opt/php7.1/bin/php wp-cli.phar --path=$SITE_PATH' BASE_URL='$SITE_URL' bash -s " < ./scripts/wp-install-plugins.sh
+ssh $SSH_USER@$SSH_HOST "cd $SITE_PATH && WPCLI='$WP_COMMAND' BASE_URL='$SITE_URL' bash -s " -- < ./scripts/wp-install-plugins.sh
 
 # TODO: Fix file permissions
