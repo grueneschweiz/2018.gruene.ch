b7485189900233b3b0f131675d7e4b6f59aa9d48
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-02-05 19:03:35 +0100

    autoformat

--- wordpress/wp-content/themes/les-verts/lib/_loader.php
+++ wordpress/wp-content/themes/les-verts/lib/_loader.php
@@ -12,8 +12,8 @@ namespace SUPT;
  * - custom rendering of a specific field
  */
 
-if( class_exists( 'acf' ) ) {
-	require_once __DIR__ .'/acf/acf-init.php';
+if ( class_exists( 'acf' ) ) {
+	require_once __DIR__ . '/acf/acf-init.php';
 	//require_once __DIR__ .'/acf/acf-content-defaults.php';
 
 	// custom locations (= where to show which field groups)
@@ -38,8 +38,8 @@ if ( class_exists('SearchWP') ) {
  *
  * Tweaks needed for polylang
  */
-if (function_exists('pll_current_language')) {
-  require_once __DIR__ . '/pll/translate-customizer.php';
+if ( function_exists( 'pll_current_language' ) ) {
+	require_once __DIR__ . '/pll/translate-customizer.php';
 }
 
 /**
@@ -88,8 +88,8 @@ if ( defined( 'WPSEO_FILE' ) ) {
  *   and should show only if certain cookie/session conditions are met
  */
 
-require_once __DIR__ .'/controllers/branding.php';
-require_once __DIR__ .'/controllers/navigation.php';
+require_once __DIR__ . '/controllers/branding.php';
+require_once __DIR__ . '/controllers/navigation.php';
 
 // register
 Branding_controller::register();
@@ -106,10 +106,10 @@ Navigation_controller::register();
  * - all of these classes have to extend the class `Model` from ./post-types/Model.php
  */
 
-require_once __DIR__ .'/post-types/PeopleType.php';
+require_once __DIR__ . '/post-types/PeopleType.php';
 
 // register post types & taxonomies
-add_action( 'init', function() {
+add_action( 'init', function () {
 	\SUPT\PeopleType::register_post_types();
 	\SUPT\PeopleType::register_taxonomy();
 } );
@@ -137,16 +137,16 @@ require_once __DIR__ . '/timber/EFPConfiguration.php';
  * Custom widgets
  */
 
-require_once __DIR__ .'/widgets/Widget.php';
-require_once __DIR__ .'/widgets/ContactWidget.php';
-require_once __DIR__ .'/widgets/ButtonWidget.php';
-require_once __DIR__ .'/widgets/LinkListWidget.php';
+require_once __DIR__ . '/widgets/Widget.php';
+require_once __DIR__ . '/widgets/ContactWidget.php';
+require_once __DIR__ . '/widgets/ButtonWidget.php';
+require_once __DIR__ . '/widgets/LinkListWidget.php';
 
 // register post types & taxonomies
-add_action( 'widgets_init', function() {
-	register_widget('\SUPT\ContactWidget');
-	register_widget('\SUPT\ButtonWidget');
-	register_widget('\SUPT\LinkListWidget');
+add_action( 'widgets_init', function () {
+	register_widget( '\SUPT\ContactWidget' );
+	register_widget( '\SUPT\ButtonWidget' );
+	register_widget( '\SUPT\LinkListWidget' );
 } );
 
 
@@ -171,7 +171,7 @@ Customizer\GetActive::register();
  * Rendering tweaks, hacks,…
  */
 
-require_once __DIR__ .'/tweaks/inject-svg-sprite.php';
+require_once __DIR__ . '/tweaks/inject-svg-sprite.php';
 
 
 /**
@@ -182,24 +182,24 @@ require_once __DIR__ .'/tweaks/inject-svg-sprite.php';
  */
 
 // functions
-require_once __DIR__ .'/twig/functions/get_lang.php';
-require_once __DIR__.'/twig/functions/register_timber_custom_post_types.php';
-require_once __DIR__.'/twig/functions/get_people.php';
-require_once __DIR__.'/twig/functions/link_props.php';
+require_once __DIR__ . '/twig/functions/get_lang.php';
+require_once __DIR__ . '/twig/functions/register_timber_custom_post_types.php';
+require_once __DIR__ . '/twig/functions/get_people.php';
+require_once __DIR__ . '/twig/functions/link_props.php';
 
 // filters
-require_once __DIR__ .'/twig/filters/email.php';
-require_once __DIR__ .'/twig/filters/phone.php';
-require_once __DIR__ .'/twig/filters/social_link.php';
-require_once __DIR__ .'/twig/filters/hexencode.php';
-require_once __DIR__ .'/twig/filters/nice_link.php';
-require_once __DIR__ .'/twig/filters/wptexturize.php';
-require_once __DIR__ .'/twig/filters/uniqueid.php';
+require_once __DIR__ . '/twig/filters/email.php';
+require_once __DIR__ . '/twig/filters/phone.php';
+require_once __DIR__ . '/twig/filters/social_link.php';
+require_once __DIR__ . '/twig/filters/hexencode.php';
+require_once __DIR__ . '/twig/filters/nice_link.php';
+require_once __DIR__ . '/twig/filters/wptexturize.php';
+require_once __DIR__ . '/twig/filters/uniqueid.php';
 
 /**
  * FORM LIBRARY
  * ============
  */
-if( class_exists( 'acf' ) ) {
+if ( class_exists( 'acf' ) ) {
 	require_once __DIR__ . '/form/_loader.php';
 }
