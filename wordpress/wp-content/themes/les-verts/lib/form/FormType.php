81dd909617107644b99403881f51fe3341e042cb
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-02-05 20:47:11 +0100

    make forms translateable

--- wordpress/wp-content/themes/les-verts/lib/form/FormType.php
+++ wordpress/wp-content/themes/les-verts/lib/form/FormType.php
@@ -5,16 +5,15 @@ namespace SUPT;
 require_once realpath( __DIR__ . '/../post-types/Model.php' );
 
 class FormType extends Model {
-	
+
 	const MODEL_NAME = 'theme_form';
 	const COLUMN_FIELD_NAME = 'fields';
-	
+
 	static function register_type() {
 		self::register_post_type();
 		self::register_acf_fields();
-		self::register_actions_filters();
 	}
-	
+
 	public static function register_post_type() {
 		register_post_type( self::MODEL_NAME,
 			array(
@@ -41,28 +40,28 @@ class FormType extends Model {
 				'publicly_queryable'  => true,
 				'show_in_nav_menus'   => false,
 				'has_archive'         => false, // false = do not generate an archive (list) page
-				'rewrite'             => array(
-					'slug'       => 'form',
-					'with_front' => false,
-					'pages'      => false,
-				),
+//				'rewrite'             => array(
+//					'slug'       => 'form',
+//					'with_front' => false,
+//					'pages'      => false,
+//				),
 				'supports'            => array(
 					'title',
-					'revisions'
+					'revisions',
 				)
 			)
 		);
 	}
-	
+
 	public static function register_acf_fields() {
 		require_once __DIR__ . '/acf/form-local-settings.php';
 		require_once __DIR__ . '/acf/input.php';
 		require_once __DIR__ . '/acf/form-details.php';
 		require_once __DIR__ . '/acf/mail-template.php';
-		
+
 		self::maybe_remove_webling_field_settings();
 	}
-	
+
 	/**
 	 * Unless api key and url are defined, remove the Webling tab from the input
 	 * field config if api key and url
@@ -70,7 +69,7 @@ class FormType extends Model {
 	private static function maybe_remove_webling_field_settings() {
 		$api_key = get_field( 'api_key', 'option' );
 		$api_url = get_field( 'api_url', 'option' );
-		
+
 		if ( ! $api_key || ! $api_url ) {
 			add_filter( 'acf/load_field/key=field_5a869960c1cf2', function ( $fields ) {
 				$fields_to_remove = [
@@ -80,22 +79,22 @@ class FormType extends Model {
 					'field_5c0fc5b4eece5',
 					'field_5c0fc84a086db',
 				];
-				
+
 				foreach ( $fields['sub_fields'] as $key => &$field ) {
 					if ( in_array( $field['key'], $fields_to_remove ) ) {
 						unset( $fields['sub_fields'][ $key ] );
 					}
 				}
-				
+
 				return $fields;
 			} );
 		}
 	}
-	
+
 	public static function register_actions_filters() {
 		add_action( 'admin_enqueue_scripts',
 			array( __CLASS__, 'admin_enqueue_scripts' ) );
-		
+
 		add_filter( 'manage_edit-' . self::MODEL_NAME . '_columns',
 			array( __CLASS__, 'register_columns_fields' ) );
 		add_action( 'manage_' . self::MODEL_NAME . '_posts_custom_column',
@@ -103,28 +102,50 @@ class FormType extends Model {
 			10, 2 );
 		add_action( 'request',
 			array( __CLASS__, 'order_by_title' ) );
-		
+
 		add_action( 'admin_menu',
 			array( __CLASS__, 'add_sent_page_menu' ) );
-		
+
 		add_filter( 'set-screen-option',
 			array( __CLASS__, 'save_submissions_per_page_option' ), 10, 3 );
+
+		add_filter( 'pll_get_post_types', array( __CLASS__, 'enable_polylang_support' ), 10, 2 );
 	}
-	
+
+	/**
+	 * Make post type translatable with polylang
+	 *
+	 * @param array $post_types the array of post type names
+	 * @param bool $is_settings true when displaying the list of custom post types in Polylang settings
+	 *
+	 * @return array
+	 */
+	public static function enable_polylang_support( $post_types, $is_settings ) {
+		if ( $is_settings ) {
+			// hides $t from the list of custom post types in Polylang settings
+			unset( $post_types[ self::MODEL_NAME ] );
+		} else {
+			// enables language and translation management for $t
+			$post_types[] = self::MODEL_NAME;
+		}
+
+		return $post_types;
+	}
+
 	/**
 	 * Enqueue custom scripts & styles
 	 */
 	public static function admin_enqueue_scripts() {
-		
+
 		if ( self::MODEL_NAME != get_post_type() ) {
 			return;
 		}
-		
+
 		$folder_uri = get_template_directory_uri() . '/lib/form';
 		wp_enqueue_script( 'form_populate_placeholder_tags', "$folder_uri/scripts/input-populate-placeholder-tags.js",
 			false, THEME_VERSION, true );
 	}
-	
+
 	/**
 	 * Add extra columns in edit table
 	 *
@@ -139,7 +160,7 @@ class FormType extends Model {
 			array_slice( $columns, 2 )
 		);
 	}
-	
+
 	/**
 	 * Populate extra columns with specific content, if available
 	 *
@@ -149,14 +170,14 @@ class FormType extends Model {
 	public static function populate_columns_fields( $column_name, $post_id ) {
 		if ( $column_name == self::COLUMN_FIELD_NAME ) {
 			$fields = get_field( 'form_fields', $post_id );
-			
+
 			echo implode( ', ', array_map( function ( $f ) {
 				return $f['form_input_label'] . ( $f['form_input_required'] ? '*' : '' );
 			}, $fields ) );
-			
+
 		}
 	}
-	
+
 	/**
 	 * Set the default order to "Title ASC" if no order set
 	 *
@@ -169,20 +190,20 @@ class FormType extends Model {
 		if ( ! is_admin() || $pagenow != 'edit.php' || $post_type != self::MODEL_NAME ) {
 			return $query_vars;
 		}
-		
+
 		if ( empty( filter_input( INPUT_GET, 'orderby' ) ) && empty( filter_input( INPUT_GET, 'order' ) ) ) {
 			$query_vars['orderby'] = 'title';
 			$query_vars['order']   = 'ASC';
 		}
-		
+
 		return $query_vars;
 	}
-	
+
 	public static function add_sent_page_menu() {
 		// add page
 		$hook = add_submenu_page( 'edit.php?post_type=' . self::MODEL_NAME, __( 'Submissions', THEME_DOMAIN ),
 			__( 'Submissions', THEME_DOMAIN ), 'edit_pages', 'submissions', array( __CLASS__, 'display_submissions_page' ) );
-		
+
 		// add screen option
 		add_action( "load-$hook", function () {
 			$args = [
@@ -193,20 +214,20 @@ class FormType extends Model {
 			add_screen_option( 'per_page', $args );
 		} );
 	}
-	
+
 	public static function save_submissions_per_page_option( $status, $option, $value ) {
 		if ( 'submissions_per_page' == $option ) {
 			return $value;
 		}
 	}
-	
+
 	public static function display_submissions_page() {
 		require_once 'helpers' . DIRECTORY_SEPARATOR . 'class-wp-list-table.php';
 		require_once 'Submissions_Table.php';
-		
+
 		$submissions = new Submissions_Table();
 		$submissions->prepare_items();
-		
+
 		?>
 		<div class="wrap">
 			<h1 class="wp-heading-inline"><?php _e( 'Form Submissions', THEME_DOMAIN ); ?></h1>
@@ -214,9 +235,9 @@ class FormType extends Model {
 			<form method="get">
 				<input type="hidden" name="post_type" value="<?php echo FormType::MODEL_NAME ?>">
 				<input type="hidden" name="page" value="submissions">
-				<?php if (!empty($_REQUEST['form_id'])) {
-					echo '<input type="hidden" name="form_id" value="'.absint($_REQUEST['form_id']).'"">';
-				}	?>
+				<?php if ( ! empty( $_REQUEST['form_id'] ) ) {
+					echo '<input type="hidden" name="form_id" value="' . absint( $_REQUEST['form_id'] ) . '"">';
+				} ?>
 				<?php $submissions->display() ?>
 			</form>
 		</div>
--- wordpress/wp-content/themes/les-verts/lib/form/_loader.php
+++ wordpress/wp-content/themes/les-verts/lib/form/_loader.php
@@ -6,7 +6,8 @@ require_once __DIR__ . '/FormType.php';
 require_once __DIR__ . '/settings-page.php';
 require_once __DIR__ . '/submission.php';
 
-// Load the Custom post Tyle
+// Load the Custom post Type
 add_action( 'init', array( '\SUPT\FormType', 'register_type' ) );
+\SUPT\FormType::register_actions_filters();
 
 new SUPT\FormSubmission();
