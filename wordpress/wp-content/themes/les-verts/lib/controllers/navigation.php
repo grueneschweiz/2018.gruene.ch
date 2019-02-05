af4f253997ab018eaab216a4422f8485f2c22b49
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-02-05 14:42:39 +0100

    events in nav: only show upcoming

--- wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
+++ wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
@@ -3,12 +3,12 @@
 namespace SUPT;
 
 class Navigation_controller {
-	
+
 	public static function register() {
 		add_filter( 'timber_context', array( __CLASS__, 'add_to_context' ) );
 		add_filter( 'wp_get_nav_menu_items', array( __CLASS__, 'add_events_to_nav' ) );
 	}
-	
+
 	/**
 	 * Detect the Automatic Agenda menu item and add all upcoming events as
 	 * sub items. Change the url of the parent item to lead to the list of
@@ -19,21 +19,21 @@ class Navigation_controller {
 	 * @return array
 	 */
 	public static function add_events_to_nav( $items ) {
-		if ( is_admin() || ! defined('TRIBE_EVENTS_FILE') ) {
+		if ( is_admin() || ! defined( 'TRIBE_EVENTS_FILE' ) ) {
 			return $items;
 		}
-		
+
 		foreach ( $items as &$item ) {
 			if ( '#supt_agenda' == $item->url ) {
 				$item->url = tribe_get_events_link();
-				$events    = tribe_get_events();
-				
+				$events    = tribe_get_events( array( 'start_date' => date( 'Y-m-d' ) ) );
+
 				foreach ( $events as $key => &$event ) {
 					$event->menu_item_parent = $item->ID;
 					$event->object_id        = (string) $event->ID;
 					$event->db_id            = $event->ID;
 					$event->classes          = array();
-					
+
 					$date         = date(
 						_x( 'y-m-d', 'short date format', THEME_DOMAIN ),
 						strtotime( $event->EventStartDate )
@@ -43,36 +43,36 @@ class Navigation_controller {
 						$date,
 						$event->post_title
 					);
-					
+
 					array_push( $items, $event );
 				}
 			}
 		}
-		
+
 		return $items;
 	}
-	
+
 	public static function add_to_context( $context ) {
-		
+
 		$menu = new \Timber\Menu( 'main-nav', [ 'depth' => 3 ] );
 		self::prepare_special_menu_items( $menu->items );
-		
+
 		$context['menu']['main']        = $menu;
 		$context['menu']['language']    = new \Timber\Menu( 'language-nav', [ 'depth' => 1 ] );
 		$context['menu']['cta']         = self::get_menu_cta();
 		$context['menu']['footer']      = new \Timber\Menu( 'footer-nav', [ 'depth' => 2 ] );
 		$context['menu']['footer_meta'] = new \Timber\Menu( 'footer-meta-nav', [ 'depth' => 1 ] );
-		
+
 		$img_id                  = get_theme_mod( Customizer\Logo::SETTING_LOGO_DARK, false );
 		$context['menu']['logo'] = [
 			'image'  => new \Timber\Image( $img_id ),
 			'srcset' => [ 2, 3 ],
 			'resize' => [ 116 ],
 		];
-		
+
 		return $context;
 	}
-	
+
 	/**
 	 * Walk the menu recursively and perform all needed custom actions
 	 *
@@ -82,26 +82,26 @@ class Navigation_controller {
 		if ( ! is_array( $timberMenu ) && ! is_object( $timberMenu ) ) {
 			return;
 		}
-		
+
 		foreach ( $timberMenu as &$item ) {
 			if ( is_array( $item ) ) {
 				foreach ( $item as &$i ) {
 					self::prepare_special_menu_items( $i );
 				}
 			}
-			
+
 			if ( $item instanceof \Timber\MenuItem ) {
-				
+
 				// custom actions
 				self::add_featured_item( $item );
-				
+
 				if ( $item->children ) {
 					self::prepare_special_menu_items( $item->children );
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Transform all category ids that belong to a featured image into a
 	 * TimberTerm and all images in a TimberImage.
@@ -114,7 +114,7 @@ class Navigation_controller {
 			$item->image    = new \Timber\Image( $item->image );
 		}
 	}
-	
+
 	/**
 	 * Return array with label and link for the get active button.
 	 *
@@ -125,17 +125,17 @@ class Navigation_controller {
 	 */
 	public static function get_menu_cta() {
 		$link = get_theme_mod( Customizer\GetActive::SETTING_GET_ACTIVE_LINK, false );
-		
+
 		if ( ! $link ) {
 			return [];
 		}
-		
+
 		$label = get_theme_mod( Customizer\GetActive::SETTING_GET_ACTIVE_LABEL, false );
-		
+
 		if ( ! $label ) {
 			$label = __( 'Get Active', THEME_DOMAIN );
 		}
-		
+
 		return [
 			'label' => $label,
 			'link'  => $link,
