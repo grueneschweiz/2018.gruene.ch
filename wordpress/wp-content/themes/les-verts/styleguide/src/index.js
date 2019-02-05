15dac11e561af491d92a438022c7e2783ba44419
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-02-05 15:42:22 +0100

    add top margin for jump links

--- wordpress/wp-content/themes/les-verts/styleguide/src/index.js
+++ wordpress/wp-content/themes/les-verts/styleguide/src/index.js
@@ -8,3 +8,6 @@ import load_components from './components/_component-loader';
 
 // Load all the components
 load_components();
+
+// Fix top offset for anchor links (fixed top nav)
+require('service/anchor.js');
---
+++ wordpress/wp-content/themes/les-verts/styleguide/src/js/service/anchor.js
@@ -0,0 +1,96 @@
+/**
+ * Make sure anchor links jump dont jump to high (fixed header)
+ *
+ * Adaptation of https://stackoverflow.com/a/13067009
+ */
+(function(document, history, location) {
+	var HISTORY_SUPPORT = !!(history && history.pushState);
+
+	var anchorScrolls = {
+		ANCHOR_REGEX: /^#[^ ]+$/,
+		ADDITIONAL_MARGIN_PX: 20,
+
+		/**
+		 * Establish events, and fix initial scroll position if a hash is provided.
+		 */
+		init: function() {
+			this.scrollToCurrent();
+			window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
+			document.body.addEventListener('click', this.delegateAnchors.bind(this));
+		},
+
+		/**
+		 * Return the offset amount to deduct from the normal scroll position.
+		 * Modify as appropriate to allow for dynamic calculations
+		 */
+		getFixedOffset: function() {
+			const nav = document.getElementsByClassName('o-header__menu');
+			const admin = document.getElementById('wpadminbar');
+			let offset = this.ADDITIONAL_MARGIN_PX;
+
+			if (admin && admin.offsetHeight) {
+				offset += admin.offsetHeight;
+			}
+
+			if (nav.length && nav[0].offsetHeight) {
+				offset += nav[0].offsetHeight;
+			}
+
+			return offset;
+		},
+
+		/**
+		 * If the provided href is an anchor which resolves to an element on the
+		 * page, scroll to it.
+		 * @param  {String} href
+		 * @return {Boolean} - Was the href an anchor.
+		 */
+		scrollIfAnchor: function(href, pushToHistory) {
+			var match, rect, anchorOffset;
+
+			if(!this.ANCHOR_REGEX.test(href)) {
+				return false;
+			}
+
+			match = document.getElementById(href.slice(1));
+
+			if(match) {
+				rect = match.getBoundingClientRect();
+				anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
+				window.scrollTo(window.pageXOffset, anchorOffset);
+
+				// Add the state to history as-per normal anchor links
+				if(HISTORY_SUPPORT && pushToHistory) {
+					history.pushState({}, document.title, location.pathname + href);
+				}
+			}
+
+			return !!match;
+		},
+
+		/**
+		 * Attempt to scroll to the current location's hash.
+		 */
+		scrollToCurrent: function() {
+			this.scrollIfAnchor(window.location.hash);
+		},
+
+		/**
+		 * If the click event's target was an anchor, fix the scroll position.
+		 */
+		delegateAnchors: function(e) {
+			var elem = e.target;
+
+			if(
+				elem.nodeName === 'A' &&
+				this.scrollIfAnchor(elem.getAttribute('href'), true)
+			) {
+				e.preventDefault();
+			}
+		}
+	};
+
+	window.addEventListener(
+		'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+	);
+})(window.document, window.history, window.location);
