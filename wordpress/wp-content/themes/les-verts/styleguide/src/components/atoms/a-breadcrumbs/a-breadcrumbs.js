f52a966fad8de6c37b9a0fffa271370b8f6494e3
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-02-28 13:49:59 +0100

    add mobile support

--- wordpress/wp-content/themes/les-verts/styleguide/src/components/atoms/a-breadcrumbs/a-breadcrumbs.js
+++ wordpress/wp-content/themes/les-verts/styleguide/src/components/atoms/a-breadcrumbs/a-breadcrumbs.js
@@ -3,12 +3,17 @@ import BaseView from 'base-view';
 const FIRST_LINK_SELECTOR = 'div > span > span > a';
 const MENU_SELECTOR = '.o-header__display';
 const ITEM_SELECTOR = '.m-menu__nav-link--';
+const HAMBURGER_SELECTOR = '.a-hamburger';
 
 const PARENT_CLASS = 'm-menu__nav-link--parent';
 
 const HASH_PREFIX = '#menu-item-';
 
 export default class ABreadcrumbs extends BaseView {
+	initialize() {
+		this.menu = document.querySelector(MENU_SELECTOR);
+	}
+
 	bind() {
 		super.bind();
 
@@ -19,12 +24,19 @@ export default class ABreadcrumbs extends BaseView {
 		const hash = event.delegateTarget.hash;
 		const id = hash.replace(HASH_PREFIX, '');
 
-		const menu = document.querySelector(MENU_SELECTOR);
-		const item = menu.querySelector(ITEM_SELECTOR + id);
+		const item = this.menu.querySelector(ITEM_SELECTOR + id);
 
 		if (item && item.classList.contains(PARENT_CLASS)) {
-			item.click();
+			this.openNav(item);
 			event.preventDefault();
 		}
+	}
+
+	openNav(item) {
+		if (!this.isDesktop()) {
+			this.menu.querySelector(HAMBURGER_SELECTOR).click();
+		}
+
+		item.click();
 	}
 }
