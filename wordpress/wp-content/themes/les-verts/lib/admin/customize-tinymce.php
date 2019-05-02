76128c01afa97e48e06b042a78d98a4915299dbb
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-05-02 19:57:34 +0200

    cleanup

--- wordpress/wp-content/themes/les-verts/lib/admin/customize-tinymce.php
+++ wordpress/wp-content/themes/les-verts/lib/admin/customize-tinymce.php
@@ -3,7 +3,7 @@
 namespace SUPT;
 
 // customize toolbars
-add_filter( 'acf/fields/wysiwyg/toolbars', function( $toolbars ) {
+add_filter( 'acf/fields/wysiwyg/toolbars', function ( $toolbars ) {
 	$toolbars['Full'] = array(
 		1 => array(
 			'formatselect',
@@ -36,16 +36,17 @@ add_filter( 'acf/fields/wysiwyg/toolbars', function( $toolbars ) {
 			'wp_help',
 		),
 	);
-	
+
 	return $toolbars;
 } );
 
 
 // remove unused block formats
-add_filter('tiny_mce_before_init', function($init) {
+add_filter( 'tiny_mce_before_init', function ( $init ) {
 	$init['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;';
+
 	return $init;
-});
+} );
 
 
 // allow &nbsp; and &shy;
@@ -57,46 +58,50 @@ add_filter( 'tiny_mce_before_init', function ( $init ) {
 } );
 
 // custom js
-add_action( 'acf/input/admin_footer', function() {
+add_action( 'acf/input/admin_footer', function () {
 	?>
 	<script type="text/javascript">
-		acf.add_filter('wysiwyg_tinymce_settings', function( mceInit, id, $field ) {
+		acf.add_filter( 'wysiwyg_tinymce_settings', function( mceInit, id, $field ) {
 			mceInit.style_formats = [
 				{
-					title: "Button Green",
-					selector: "a",
-					classes: "a-button a-button--primary"
+					title: 'Button Green',
+					selector: 'a',
+					classes: 'a-button a-button--primary',
 				},
 				{
-					title: "Button Magenta",
-					selector: "a",
-					classes: "a-button a-button--secondary"
+					title: 'Button Magenta',
+					selector: 'a',
+					classes: 'a-button a-button--secondary',
 				},
 				{
-					title: "Button Outline Green",
-					selector: "a",
-					classes: "a-button a-button--primary a-button--outline"
+					title: 'Button Outline Green',
+					selector: 'a',
+					classes: 'a-button a-button--primary a-button--outline',
 				},
 				{
-					title: "Button Outline Magenta",
-					selector: "a",
-					classes: "a-button a-button--secondary a-button--outline"
-				}
+					title: 'Button Outline Magenta',
+					selector: 'a',
+					classes: 'a-button a-button--secondary a-button--outline',
+				},
 			];
 			return mceInit;
-		});
+		} );
 	</script>
 	<?php
 } );
 
 // custom css
-add_filter( 'tiny_mce_before_init', function( $mce_init ) {
+add_filter( 'tiny_mce_before_init', function ( $mce_init ) {
 	$styleguide_css = get_stylesheet_directory_uri() . '/static/style' . ( WP_DEBUG ? '' : '.min' ) . '.css';
-	$content_css = get_stylesheet_directory_uri() . '/style-admin-editor.css';
-	if (isset($mce_init['content_css'])) {
-		$content_css_new =  $mce_init[ 'content_css' ].','.$styleguide_css.','.$content_css;
+	$content_css    = get_stylesheet_directory_uri() . '/style-admin-editor.css';
+
+	if ( isset( $mce_init['content_css'] ) ) {
+		$styles = $mce_init['content_css'] . ',' . $styleguide_css . ',' . $content_css;
+	} else {
+		$styles = $styleguide_css . ',' . $content_css;
 	}
-	$mce_init[ 'content_css' ] = $content_css_new;
+
+	$mce_init['content_css'] = $styles;
 
 	return $mce_init;
 } );
