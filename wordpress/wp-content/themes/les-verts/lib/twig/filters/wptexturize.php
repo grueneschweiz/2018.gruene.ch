646f899585390a42ba05b191488cc2d8ba51203a
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-09-30 16:12:21 +0200

    auto add no-break spaces (used especially for french content)

--- wordpress/wp-content/themes/les-verts/lib/twig/filters/wptexturize.php
+++ wordpress/wp-content/themes/les-verts/lib/twig/filters/wptexturize.php
@@ -2,13 +2,23 @@
 
 namespace SUPT;
 
-use Twig_SimpleFilter;
+use Twig\TwigFilter;
 
 add_filter( 'get_twig', function ( $twig ) {
 	$twig->addFilter(
-		new Twig_SimpleFilter( 'wptexturize', function ( $string ) {
+		new TwigFilter( 'wptexturize', function ( $string ) {
+			$string = wptexturize( $string );
+
+			$simple_chars = '?!:';
+			$raquo = '&raquo;|&#187;|\x{00bb}';
+			$rsaquo ='&rsaquo;|&#8250;|\x{203a}';
+			$string = preg_replace("/ ([$simple_chars]|$raquo|$rsaquo)/u", "&nbsp;$1", $string);
+
+			$laquo = '&laquo;|&#171;|\x{00ab}';
+			$lsaquo ='&lsaquo;|&#8249;|\x{2039}';
+			$string = preg_replace("/($laquo|$lsaquo) /u", "$1&nbsp;", $string);
 			
-			return wptexturize( $string );
+			return $string;
 		} )
 	);
 	
