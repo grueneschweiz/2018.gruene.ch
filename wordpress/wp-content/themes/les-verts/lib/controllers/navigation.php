d280ba39fd68622ba8422498af5edab94774a337
cyrillbolliger <cyrill.bolliger@gmail.com>
2020-03-10 18:53:27 +0100

    fix: trouble with tribe events > 5.0.* and broken urls that return a white page

--- wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
+++ wordpress/wp-content/themes/les-verts/lib/controllers/navigation.php
@@ -31,7 +31,9 @@ class Navigation_controller {
 		foreach ( $items as &$item ) {
 
 			if ( '#supt_agenda' == $item->url ) {
-				$item->url = tribe_get_listview_link( false );
+				// trailing slash leads to bug in tribe events 5.0.* (white page)
+				$item->url = rtrim( tribe_get_listview_link( false ), '/' );
+
 				$events    = tribe_get_events( array(
 					'start_date'       => date( 'Y-m-d' ),
 					'category'         => 0,
