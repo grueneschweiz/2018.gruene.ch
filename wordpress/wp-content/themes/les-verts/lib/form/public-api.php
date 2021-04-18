4431543c632a99e8bf078db8583badbbaf9bea25
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-04-18 13:09:44 +0200

    forms: enable support for caching plugins

--- wordpress/wp-content/themes/les-verts/lib/form/public-api.php
+++ wordpress/wp-content/themes/les-verts/lib/form/public-api.php
@@ -7,14 +7,22 @@
  * This file exposes form function to use anywhere in your code
  */
 
+use SUPT\FormModel;
+use SUPT\Nonce;
+
 /**
  * Get a form submission nonce
  *
  * @return string
  */
 function supt_theme_form_create_nonce() {
+	if ( ! defined( 'DONOTCACHEPAGE' ) ) {
+		define( 'DONOTCACHEPAGE', true );
+	}
+
 	require_once __DIR__ . '/include/Nonce.php';
-	return \SUPT\Nonce::create();
+
+	return Nonce::create();
 }
 
 /**
@@ -28,7 +36,7 @@ function supt_theme_form_submission_count($form_id) {
 	require_once __DIR__ . '/include/FormModel.php';
 
 	try {
-		$form = new \SUPT\FormModel( $form_id );
+		$form = new FormModel( $form_id );
 	} catch ( Exception $e ) {
 		return -1;
 	}
