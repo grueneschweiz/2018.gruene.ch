b58fd83a8df0b367a526539dea05f55053654826
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-07-13 11:45:49 +0200

    changed: internal referred link render not distraction free unless `?focus` is present in the URL

--- wordpress/wp-content/themes/les-verts/lib/controllers/landingpage.php
+++ wordpress/wp-content/themes/les-verts/lib/controllers/landingpage.php
@@ -16,12 +16,80 @@ class Landingpage_controller {
 		'text',
 	);
 
+	/**
+	 * Controls if the page should be rendered distraction free
+	 *
+	 * @var bool
+	 */
+	private static $distraction_free;
+
 	public static function register() {
+		add_filter( 'timber_context', array( __CLASS__, 'add_to_context' ) );
 		add_filter( 'sidebars_widgets', array( __CLASS__, 'filter_widgets' ) );
 	}
 
-	public static function filter_widgets( array $widgets ): array {
+	public static function add_to_context( $context ) {
+		$context['distraction_free'] = self::should_be_distraction_free();
+
+		return $context;
+	}
+
+	/**
+	 * Determines if the page should be displayed distraction free.
+	 *
+	 * Returns true if the page uses the single_landingpage.php template
+	 * AND satisfies any of the following criteria:
+	 * - the url query string `focus` is present
+	 * - the referrer is not the current site
+	 *
+	 * Therefore visitors following internal links do not see the
+	 * distraction free page, unless the `focus` query string was set.
+	 *
+	 * Subsequent calls of this function return a cached result.
+	 *
+	 * @return bool
+	 */
+	public static function should_be_distraction_free() {
+		if ( isset( self::$distraction_free ) ) {
+			return self::$distraction_free;
+		}
+
 		if ( ! is_page_template( 'single_landingpage.php' ) ) {
+			self::$distraction_free = false;
+
+			return self::$distraction_free;
+		}
+
+		if ( isset( $_GET['focus'] ) ) {
+			self::$distraction_free = true;
+
+			return self::$distraction_free;
+		}
+
+		if ( empty( $_SERVER['HTTP_REFERER'] ) ) {
+			self::$distraction_free = true;
+
+			return self::$distraction_free;
+		}
+
+		$referer = parse_url( $_SERVER['HTTP_REFERER'], PHP_URL_HOST );
+		$site    = parse_url( site_url(), PHP_URL_HOST );
+
+		self::$distraction_free = $referer !== $site;
+
+		return self::$distraction_free;
+	}
+
+	/**
+	 * Removes any widget that is not explicitly allowed if the page
+	 * is rendered distraction free.
+	 *
+	 * @param array $widgets
+	 *
+	 * @return array
+	 */
+	public static function filter_widgets( array $widgets ): array {
+		if ( ! self::should_be_distraction_free() ) {
 			return $widgets;
 		}
 
--- wordpress/wp-content/themes/les-verts/single_landingpage.php
+++ wordpress/wp-content/themes/les-verts/single_landingpage.php
@@ -9,6 +9,5 @@ use SUPT\ACFPost;
 $context                 = Timber::get_context();
 $post                    = new ACFPost();
 $context['post']         = $post;
-$context['landing_page'] = true;
 $templates               = array( 'single.twig' );
 Timber::render( $templates, $context );
