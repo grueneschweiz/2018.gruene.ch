30cfaaa8cfb94a42189b632e0df241caa046bc4d
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2021-09-30 16:22:27 +0200

    improved error logging

--- wordpress/wp-content/themes/les-verts/lib/form/include/CrmDao.php
+++ wordpress/wp-content/themes/les-verts/lib/form/include/CrmDao.php
@@ -164,7 +164,7 @@ class CrmDao {
 		if ( $resp->get_status() !== 201 ) {
 			$status_code = $this->is_timeout_error( $resp->get_data() ) ? 408 : $resp->get_status();
 
-			$data_sent = print_r( $crm_data, true );
+			$data_sent = print_r( $this->redactToken( $args ), true );
 			throw new Exception( "Could not save member to crm. Crm returned status code: {$resp->get_status()}. Reason: {$resp->get_data()}.\n\nData sent: {$data_sent}", $status_code );
 		}
 
@@ -216,7 +216,7 @@ class CrmDao {
 		if ( $resp->get_status() !== 200 ) {
 			$status_code = $this->is_timeout_error( $resp->get_data() ) ? 408 : $resp->get_status();
 
-			$data_sent = print_r( $crm_data, true );
+			$data_sent = print_r( $this->redactToken( $args ), true );
 			throw new Exception( "Could match member in crm. Crm returned status code: {$resp->get_status()}. Reason: {$resp->get_data()}.\n\nData sent: {$data_sent}", $status_code );
 		}
 
@@ -224,7 +224,7 @@ class CrmDao {
 
 		if ( ! array_key_exists( 'status', $body ) ) {
 			$body_string = print_r( $body, true );
-			$data_sent   = print_r( $crm_data, true );
+			$data_sent   = print_r( $this->redactToken( $args ), true );
 			throw new Exception( "Invalid response from member match endpoint of crm wrapper. Response: {$body_string}. \n\nData sent: {$data_sent}" );
 		}
 
@@ -294,5 +294,21 @@ class CrmDao {
 		$resp = $response['http_response'];
 
 		return 200 === $resp->get_status();
+	}
+
+	/**
+	 * Censor Authorization header from given wp_remote_* args.
+	 * Use for logging.
+	 *
+	 * @param array $args
+	 *
+	 * @return array
+	 */
+	private function redactToken( array $args ): array {
+		if ( isset( $args['headers']['Authorization'] ) ) {
+			$args['headers']['Authorization'] = 'REDACTED';
+		}
+
+		return $args;
 	}
 }
