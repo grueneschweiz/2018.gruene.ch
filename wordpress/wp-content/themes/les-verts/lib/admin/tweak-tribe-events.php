ccdc63449a4cc8671f9500cd5f7249cf634a95d0
Cyrill Bolliger <mail@cyrill.me>
2022-04-19 12:18:47 +0200

    [FEAT] Disable the tribe events customizer

--- wordpress/wp-content/themes/les-verts/lib/admin/tweak-tribe-events.php
+++ wordpress/wp-content/themes/les-verts/lib/admin/tweak-tribe-events.php
@@ -3,6 +3,14 @@
 /**
  * Remove the option meta box of tribe events, as these options are not supported
  */
-add_action( 'do_meta_boxes', function() {
-	remove_meta_box('tribe_events_event_options', 'tribe_events', 'side');
+add_action( 'do_meta_boxes', function () {
+	remove_meta_box( 'tribe_events_event_options', 'tribe_events', 'side' );
 } );
+
+/**
+ * Disable the tribe events customizer
+ */
+add_action( 'customize_register', function ( $wp_customize ) {
+	$wp_customize->remove_panel( 'tribe_customizer' );
+}, 99 );
+
