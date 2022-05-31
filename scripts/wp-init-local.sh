06c98b8183f3fe5a9ae36468696c42e74cfcd593
Cyrill Bolliger <mail@cyrill.me>
2022-05-31 11:07:46 +0200

    [FIX] install scripts (closes #130)

--- scripts/wp-init-local.sh
+++ scripts/wp-init-local.sh
@@ -14,8 +14,8 @@ docker exec wp_docker_les_verts wp core install --url=localhost --title=LesVERTS
 # Run the bootstrap script
 docker cp scripts/wp-install-plugins.sh wp_docker_les_verts:/var/www/html/wp-install-plugins.sh
 docker cp scripts/wp-configure.sh wp_docker_les_verts:/var/www/html/wp-configure.sh
-docker exec wp_docker_les_verts bash -c "chmod +x wp-install-plugins.sh && WPCLI='wp --color' ./wp-install-plugins.sh -l"
-docker exec wp_docker_les_verts bash -c "chmod +x wp-configure.sh && WPCLI='wp --color' ./wp-configure.sh"
+docker exec wp_docker_les_verts bash -c "chmod +x wp-install-plugins.sh && export WPCLI='wp --color'; ./wp-install-plugins.sh -l"
+docker exec wp_docker_les_verts bash -c "chmod +x wp-configure.sh && export WPCLI='wp --color'; ./wp-configure.sh"
 
 # Build dependencies so wo can make the symlink for the static files
 yarn install && yarn build
