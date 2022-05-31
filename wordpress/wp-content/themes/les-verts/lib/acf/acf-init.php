910a6565748319f3cb1e448482d3c8cf84484de2
Cyrill Bolliger <mail@cyrill.me>
2022-05-31 11:28:11 +0200

    [FEAT] remove ACF code block if acf-code-field plugin is missing

--- wordpress/wp-content/themes/les-verts/lib/acf/acf-init.php
+++ wordpress/wp-content/themes/les-verts/lib/acf/acf-init.php
@@ -2,9 +2,20 @@
 
 /**
  * Set ACF settings on init
- * 
+ *
  * Read more: https://www.advancedcustomfields.com/resources/acf-settings/
  */
-add_action( 'acf/init', function() {
-	acf_update_setting('l10n_textdomain', THEME_DOMAIN);
+add_action( 'acf/init', function () {
+	acf_update_setting( 'l10n_textdomain', THEME_DOMAIN );
+} );
+
+/**
+ * Remove code block, if acf-code-field plugin is missing
+ */
+add_filter( 'acf/load_field/key=field_5cee95b5caff7', function ( $field ) {
+	if ( ! is_plugin_active( 'acf-code-field/acf-code-field.php' ) ) {
+		unset( $field['layouts']['layout_5c4f21cbd787c'] );
+	}
+
+	return $field;
 } );
