c3587468c89a4ba81c96228fd77b6ef8646896f9
Cyrill Bolliger <mail@cyrill.me>
2022-07-19 18:22:10 +0200

    [FIX] fatal error when an event was not found (closes #147)

--- wordpress/wp-content/themes/les-verts/404.php
+++ wordpress/wp-content/themes/les-verts/404.php
@@ -9,13 +9,25 @@
  * @since    Timber 0.1
  */
 
+use SUPT\SUPTPostQuery;
+
 $context = Timber::get_context();
 
-$posts = new \SUPT\SUPTPostQuery( array(
-	'post_type'           => 'post',
-	'orderby'             => 'date',
-	'ignore_sticky_posts' => true,
-) );
+if ( isset( $post_type ) && 'tribe_events' === $post_type ) {
+	$posts = tribe_get_events( array(
+		'eventDisplay'   => tribe_get_request_var(
+			'tribe_event_display',
+			'list'
+		),
+		'posts_per_page' => - 1,
+	) );
+} else {
+	$posts = new SUPTPostQuery( array(
+		'post_type'           => 'post',
+		'orderby'             => 'date',
+		'ignore_sticky_posts' => true,
+	) );
+}
 
 $context['posts']      = $posts;
 $context['title']      = __( 'Latest posts', THEME_DOMAIN );
