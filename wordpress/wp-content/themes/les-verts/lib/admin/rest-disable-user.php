df82e06b4bb66ee0af16d44fa1a1a705a0a65998
Cyrill Bolliger <mail@cyrill.me>
2023-07-30 18:21:55 +0200

    [FEAT] allow /wp/v2/users for authorized users

--- wordpress/wp-content/themes/les-verts/lib/admin/rest-disable-user.php
+++ wordpress/wp-content/themes/les-verts/lib/admin/rest-disable-user.php
@@ -1,11 +1,14 @@
 <?php
 
 add_filter( 'rest_endpoints', function( $endpoints ) {
-	if ( isset( $endpoints['/wp/v2/users'] ) ) {
-		unset( $endpoints['/wp/v2/users'] );
+	if ( ! current_user_can( 'list_users' ) ) {
+		if ( isset( $endpoints['/wp/v2/users'] ) ) {
+			unset( $endpoints['/wp/v2/users'] );
+		}
+		if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+			unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+		}
 	}
-	if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
-		unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
-	}
+
 	return $endpoints;
 });
