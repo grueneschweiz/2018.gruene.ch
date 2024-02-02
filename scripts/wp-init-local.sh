884da889524fa2400538eff064c32bac968b3193
Cyrill Bolliger <mail@cyrill.me>
2024-02-02 15:23:06 +0100

    [FIX] wp cli cache dir

--- scripts/wp-init-local.sh
+++ scripts/wp-init-local.sh
@@ -14,8 +14,8 @@ docker exec wp_docker_les_verts wp core install --url=localhost --title=LesVERTS
 # Run the bootstrap script
 docker cp scripts/wp-install-plugins.sh wp_docker_les_verts:/var/www/html/wp-install-plugins.sh
 docker cp scripts/wp-configure.sh wp_docker_les_verts:/var/www/html/wp-configure.sh
-docker exec wp_docker_les_verts bash -c "chmod +x wp-install-plugins.sh && export WPCLI='wp --color'; ./wp-install-plugins.sh -l"
-docker exec wp_docker_les_verts bash -c "chmod +x wp-configure.sh && export WPCLI='wp --color'; ./wp-configure.sh"
+docker exec wp_docker_les_verts bash -c "chmod +x wp-install-plugins.sh && export WPCLI='wp --color'; export WP_CLI_CACHE_DIR=/home/www-data; ./wp-install-plugins.sh -l"
+docker exec wp_docker_les_verts bash -c "chmod +x wp-configure.sh && export WPCLI='wp --color'; export WP_CLI_CACHE_DIR=/home/www-data; ./wp-configure.sh"
 
 # Build dependencies so wo can make the symlink for the static files
 yarn install && yarn build
@@ -26,7 +26,7 @@ docker exec --user $(id -u):$(id -g) wp_docker_les_verts bash -c "cd wp-content/
 # Import demo content
 docker cp scripts/wp-add-demo-content.sh wp_docker_les_verts:/var/www/html/wp-add-demo-content.sh
 docker exec wp_docker_les_verts bash -c "WPCLI='wp --color' wp plugin activate wordpress-importer"
-docker exec wp_docker_les_verts bash -c "chmod +x wp-add-demo-content.sh && WPCLI='wp --color' ./wp-add-demo-content.sh"
+docker exec wp_docker_les_verts bash -c "chmod +x wp-add-demo-content.sh && WP_CLI_CACHE_DIR=/home/www-data WPCLI='wp --color' ./wp-add-demo-content.sh"
 
 ######################
 # ENABLE IDE SUPPORT #
