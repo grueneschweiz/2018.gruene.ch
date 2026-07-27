95adcee34b7bf4709bd2f01c658f95687e4ffd87
Michael Schär <michael.schaer@gruene.ch>
2026-07-27 16:21:41 +0200

    fix: change network option commands to site option for limit login attempts configuration

--- scripts/wp-configure.sh
+++ scripts/wp-configure.sh
@@ -103,13 +103,13 @@ $WPCLI option patch insert openid_connect_generic_settings create_if_does_not_ex
 
 # configure limit login attempts
 if [ $NETWORK ]; then
-	$WPCLI network option set limit_login_show_top_level_menu_item '0'
-	$WPCLI network option set limit_login_hide_dashboard_widget '1'
-	$WPCLI network option set limit_login_gdpr '1'
-	$WPCLI network option set limit_login_lockout_notify ''
-	$WPCLI network option set limit_login_digest_daily '0'
-	$WPCLI network option set limit_login_digest_weekly '0'
-	$WPCLI network option set limit_login_digest_monthly '0'
+	$WPCLI site option update limit_login_show_top_level_menu_item '0'
+	$WPCLI site option update limit_login_hide_dashboard_widget '1'
+	$WPCLI site option update limit_login_gdpr '1'
+	$WPCLI site option update limit_login_lockout_notify ''
+	$WPCLI site option update limit_login_digest_daily '0'
+	$WPCLI site option update limit_login_digest_weekly '0'
+	$WPCLI site option update limit_login_digest_monthly '0'
 else
 	$WPCLI option set limit_login_show_top_level_menu_item '0'
 	$WPCLI option set limit_login_hide_dashboard_widget '1'
